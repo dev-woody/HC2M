60668da978cf01a2755748f91999a1e28f01ce6a
woodoll <87690037+woodoll@users.noreply.github.com>
2022-08-22 00:03:11 +0900

    Update Plan.js

--- src/components/Plan.js
+++ src/components/Plan.js
@@ -1,67 +1,55 @@
-import React, { useState, useEffect } from "react";
+import React, { useEffect } from "react";
 import { Fade } from "react-reveal";
 
 const Plan = ({ setBtnActive }) => {
-  const [loading, setLoading] = useState(true);
   useEffect(() => {
     setBtnActive("plan");
   }, [setBtnActive]);
 
-  useEffect(() => {
-    setLoading(false);
-  });
   return (
-    <>
-      {loading ? null : (
-        <Fade>
-          <div
-            className="container"
-            style={{ minHeight: "calc(100vh - 100px)" }}
-          >
-            <a
-              href="https://map.naver.com/v5/entry/place/1762427665?c=14139029.5974415,4478842.2189251,13,0,0,0,dh&placePath=%2Fhome%3Fentry=plt"
-              target="_blank"
-            >
-              <div id="map" className="map-box"></div>
-            </a>
-            <section className="contact_contents ">
-              <header className="contact_header">
-                <h1>CONTACT</h1>
-              </header>
-              <div className="contact_text">
-                <p className="name_en">HC2M. </p>
-                <p className="address_en">
-                  경기 수원시 팔달구 화서문로46번길 16 지하 1층
-                </p>
-                <p className="address_ja">신풍동 141</p>
-                <p className="tel">
-                  Tel : <a href="tel:01045314384">010-4531-4384</a>
-                </p>
-                <p className="mail">
-                  Mail :{" "}
-                  <a href="mailto:HC2M_CHU@naver.com">HC2M_CHU@naver.com</a>
-                </p>
-                <p className="mail">
-                  Instagram :
-                  <a href="https://instagram.com/hc2m_chu?igshid=YmMyMTA2M2Y=">
-                    @hc2m_chu
-                  </a>
-                </p>
-              </div>
-              <div className="btn_googlemap">
-                <a
-                  href="https://map.naver.com/v5/entry/place/1762427665?c=14139574.0370792,4478250.3619441,12.38,0,0,0,dh&placePath=%2Fhome%3Fentry=plt"
-                  target="_blank"
-                  rel="noreferrer"
-                >
-                  NAVER MAP
-                </a>
-              </div>
-            </section>
+    <Fade>
+      <div className="container" style={{ minHeight: "calc(100vh - 100px)" }}>
+        <a
+          href="https://map.naver.com/v5/entry/place/1762427665?c=14139029.5974415,4478842.2189251,13,0,0,0,dh&placePath=%2Fhome%3Fentry=plt"
+          target="_blank"
+        >
+          <div id="map" className="map-box"></div>
+        </a>
+        <section className="contact_contents ">
+          <header className="contact_header">
+            <h1>CONTACT</h1>
+          </header>
+          <div className="contact_text">
+            <p className="name_en">HC2M. </p>
+            <p className="address_en">
+              경기 수원시 팔달구 화서문로46번길 16 지하 1층
+            </p>
+            <p className="address_ja">신풍동 141</p>
+            <p className="tel">
+              Tel : <a href="tel:01045314384">010-4531-4384</a>
+            </p>
+            <p className="mail">
+              Mail : <a href="mailto:HC2M_CHU@naver.com">HC2M_CHU@naver.com</a>
+            </p>
+            <p className="mail">
+              Instagram :
+              <a href="https://instagram.com/hc2m_chu?igshid=YmMyMTA2M2Y=">
+                @hc2m_chu
+              </a>
+            </p>
           </div>
-        </Fade>
-      )}
-    </>
+          <div className="btn_googlemap">
+            <a
+              href="https://map.naver.com/v5/entry/place/1762427665?c=14139574.0370792,4478250.3619441,12.38,0,0,0,dh&placePath=%2Fhome%3Fentry=plt"
+              target="_blank"
+              rel="noreferrer"
+            >
+              NAVER MAP
+            </a>
+          </div>
+        </section>
+      </div>
+    </Fade>
   );
 };
 
